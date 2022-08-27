547b70c66e0b1a579e521a12a60e300d73d7de84
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-08-26 17:36:40 -0700

    It will order the questions correctly.

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// Business Logic
+//tried to play with buttons for the background, havent been able to figure it out.
+let btn1 = document.querySelector("btn-1");
+let btn2 = document.querySelector("btn-2");
+let btn3 = document.querySelector("btn-3");
+
+btn1.addEventListener("click", () =>{
+  document.body.style.backgroundImage = "url('images/1.png')"
+});
+
+btn2.addEventListener("click", () =>{
+  document.body.style.backgroundImage = "url('images/2.png')"
+});
+
+btn3.addEventListener("click", () =>{
+  document.body.style.backgroundImage = "url('images/3.jpg')"
+});
+
+function handleTest(event) {
+  event.preventDefault();
+  const operator = document.querySelector("input[name='operator']:checked").value;
+}
+
+  let result;
+  if (operator === "Yes1" && "Yes4") {
+    result = "You should look into C#!";
+  } else if (operator === "Yes1" && "Yes2") {
+    result = "You should look into java script!";
+  } else if (operator === "Yes2" && "Yes3") {
+    result = "You should look into Python!";
+  } else if (operator === "Yes3" && "Yes4") {
+    result = "You should look into Ruby!";
+  } else if (operator === "Yes5") {
+    result = "You should look into C++!";  
+  }
+    else {return}
+
+  document.getElementById("output").innerText = result;
+
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("Test");
+  form.addEventListener("submit", handleCalculation);
+});

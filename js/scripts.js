044675d655703bcfe299cd38517c654c3df96df5
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-09-07 19:55:08 -0700

    it will gather input from user

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 // Business Logic
-//tried to play with buttons for the background, havent been able to figure it out.
-window.addEventListener('load', function); {
-  const firstHeader = document.querySelector(h1);
-  firstHeader.remove();
 
+  /*
   let btn1 = document.querySelector("btn-1");
   let btn2 = document.querySelector("btn-2");
   let btn3 = document.querySelector("btn-3");
@@ -19,26 +16,53 @@ window.addEventListener('load', function); {
   btn3.addEventListener("click", () =>{
     document.body.style.backgroundImage = "url('images/3.jpg')"
   });
+*/
+  // let btn = document.querySelector(btn);
+
+  // btn.addEventListener("click")
+
+  // const operator = document.querySelector("input[name='operator']:checked").value;
+
   
-  function handleTest(event) {
-    event.preventDefault();
-    const operator = document.querySelector("input[name='operator']:checked").value;
-  }
+  // function handleTest(event) {
+
+  //   let result;
+  //   if (operator === "1") {
+  //     result = "Java";
+  //   } else if (operator === "2") {
+  //     result = "You should look into java script!";
+  //   } else if (operator === "3") {
+  //     result = "You should look into C++!";  
+  //   } else {
+  //     result = "You should look into java script!"
+  //   }
   
-    let result;
-    if (operator === "1") {
-      result = "You should look into C#!";
-    } else if (operator === "2") {
-      result = "You should look into java script!";
-    } else if (operator === "3") {
-      result = "You should look into C++!";  
-    }
-  
-    document.getElementById("output").innerText = result;
-  }
+  //   document.getElementById("output").innerText = result;
+  // }
   
   
   window.addEventListener("load", function() {
     const form = document.getElementById("Test");
-    form.addEventListener("submit", handleTest);
+    form.addEventListener("submit", function(event) { 
+      event.preventDefault();
+      const answer1 = document.querySelector("input[type=radio][name=Q1]:checked").value;
+      const answer2 = document.querySelector("input[type=radio][name=Q2]:checked").value;
+      const answer3 = document.querySelector("input[type=radio][name=Q3]:checked").value;
+      const answer4 = document.querySelector("input[type=radio][name=Q4]:checked").value;
+      const answer5 = document.querySelector("input[type=radio][name=Q5]:checked").value;
+      console.log(answer1, answer2, answer3, answer4, answer5)
+
+    let result;
+      if (answer1 === "1" && answer2 === "2" && answer3 === "3" && answer4 === "1" && answer5 === "2") {
+        result = "You should look into Python!";
+      } else if (answer1 === "2" && answer2 === "3" && answer3 === "1" && answer4 === "2" && answer5 === "3") {
+        result = "You should look into java script!";
+      } else if (answer1 === "3" && answer2 === "1" && answer3 === "2" && answer4 === "3" && answer5 === "1") {
+        result = "You should look into C++!";  
+      } else {
+        result = "You should look into java script!"
+    }
+  
+    document.getElementById("output").innerText = result;
+    });
   });

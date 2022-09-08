46a84c2a35d6536c1549c8319b6c3a8426dcb484
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-09-07 20:16:59 -0700

    it will combine the 'about' section and the 'results' section into one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,4 @@
 // Business Logic
-
-  /*
-  let btn1 = document.querySelector("btn-1");
-  let btn2 = document.querySelector("btn-2");
-  let btn3 = document.querySelector("btn-3");
-  
-  btn1.addEventListener("click", () =>{
-    document.body.style.backgroundImage = "url('images/1.png')"
-  });
-  
-  btn2.addEventListener("click", () =>{
-    document.body.style.backgroundImage = "url('images/2.png')"
-  });
-  
-  btn3.addEventListener("click", () =>{
-    document.body.style.backgroundImage = "url('images/3.jpg')"
-  });
-*/
-  // let btn = document.querySelector(btn);
-
-  // btn.addEventListener("click")
-
-  // const operator = document.querySelector("input[name='operator']:checked").value;
-
-  
-  // function handleTest(event) {
-
-  //   let result;
-  //   if (operator === "1") {
-  //     result = "Java";
-  //   } else if (operator === "2") {
-  //     result = "You should look into java script!";
-  //   } else if (operator === "3") {
-  //     result = "You should look into C++!";  
-  //   } else {
-  //     result = "You should look into java script!"
-  //   }
-  
-  //   document.getElementById("output").innerText = result;
-  // }
-  
   
   window.addEventListener("load", function() {
     const form = document.getElementById("Test");
@@ -52,6 +11,8 @@
       const answer5 = document.querySelector("input[type=radio][name=Q5]:checked").value;
       console.log(answer1, answer2, answer3, answer4, answer5)
 
+// User Interface Logic
+
     let result;
       if (answer1 === "1" && answer2 === "2" && answer3 === "3" && answer4 === "1" && answer5 === "2") {
         result = "You should look into Python!";
@@ -60,7 +21,7 @@
       } else if (answer1 === "3" && answer2 === "1" && answer3 === "2" && answer4 === "3" && answer5 === "1") {
         result = "You should look into C++!";  
       } else {
-        result = "You should look into java script!"
+        result = "You should look into java script! Check out Epicodus for a great coding school!"
     }
   
     document.getElementById("output").innerText = result;

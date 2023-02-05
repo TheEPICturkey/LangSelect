564545d36d5ec0d458d3724e0d2bacf52f228f6c
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-02-04 18:04:37 -0800

    Added another answer option

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,9 @@
       } else if (answer1 === "2" && answer2 === "3" && answer3 === "1" && answer4 === "2" && answer5 === "3") {
         result = "You should look into java script! JavaScript (often shortened to JS) is a lightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages, but it's used in many non-browser environments as well.";
       } else if (answer1 === "3" && answer2 === "1" && answer3 === "2" && answer4 === "3" && answer5 === "1") {
-        result = "You should look into C++! The programming language known as C++ (or “C-plus-plus”) is a general purpose coding language which has been used for game programming, software engineering, data structures, developing browsers, operating systems, applications, and more for over forty years.";  
+        result = "You should look into C++! The programming language known as C++ (or “C-plus-plus”) is a general purpose coding language which has been used for game programming, software engineering, data structures, developing browsers, operating systems, applications, and more for over forty years.";
+      } else if (answer1 === "1" && answer2 === "1" && answer3 === "2" && answer4 === "3" && answer5 === "2") {
+        result = "Check out EPICODUS!";  
       } else {
         result = "You should look into java script! Check out Epicodus for a great coding school! JavaScript (often shortened to JS) is a lightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages, but it's used in many non-browser environments as well."
     }
